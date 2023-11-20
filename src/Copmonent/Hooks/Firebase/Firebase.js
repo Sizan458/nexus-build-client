9206397ebec0abed59989b30708a48a6249f960d
Sizan458 <www.shijan23@gmail.com>
2023-11-20 19:48:44 +0600

    install firebase , private hooks & auth provider

---
+++ src/Copmonent/Hooks/Firebase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIkK3EbDbqaYMpb7eT7mh1wgPNbIVsNG8",
+  authDomain: "nexus-build-b7042.firebaseapp.com",
+  projectId: "nexus-build-b7042",
+  storageBucket: "nexus-build-b7042.appspot.com",
+  messagingSenderId: "1000898915647",
+  appId: "1:1000898915647:web:0c6d6b3a5260d2e099f78a"
+};
+
+// Initialize Firebase
+const App = initializeApp(firebaseConfig);
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -12,17 +12,20 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Copmonent/Hooks/AutthProvider/AuthProvider';
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   
+  <AuthProvider>
   <QueryClientProvider client={queryClient}> 
 <HelmetProvider>
 <RouterProvider router={Router}/>
 </HelmetProvider>
   </QueryClientProvider>
+  </AuthProvider>
   
   </React.StrictMode>,
 )
